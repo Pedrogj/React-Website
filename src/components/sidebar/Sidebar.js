021422e8b9cea32a3043f71b3f77b37b2c174dfd
Pedro Perez <pedrodrums2015@gmail.com>
2022-06-29 11:31:32 -0400

    working in Sidebar.js: Add style

---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,31 @@
+import {
+  SidebarContainer,
+  CloseIcon,
+  Icon,
+  SidebarWrapper,
+  SidebarMenu,
+  SidebarLink,
+  SideBtnWrap,
+  SidebarRoute,
+} from "./SidebarStyle";
+
+export const Sidebar = () => {
+  return (
+    <SidebarContainer>
+      <Icon>
+        <CloseIcon />
+      </Icon>
+      <SidebarWrapper>
+        <SidebarMenu>
+          <SidebarLink to="about">About</SidebarLink>
+          <SidebarLink to="discover">Discover</SidebarLink>
+          <SidebarLink to="services">Services</SidebarLink>
+          <SidebarLink to="signup">Sign Up</SidebarLink>
+        </SidebarMenu>
+        <SideBtnWrap>
+          <SidebarRoute to="signin">Sign In</SidebarRoute>
+        </SideBtnWrap>
+      </SidebarWrapper>
+    </SidebarContainer>
+  );
+};
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Navbar } from "../components/navbar/Navbar";
+import { Sidebar } from "../components/sidebar/Sidebar";
 import { Home } from "../pages/home/Home";
 
 export const AppRouter = () => {
   return (
     <BrowserRouter>
+      <Sidebar />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
